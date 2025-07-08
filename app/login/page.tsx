7489925e1975b42af30d46f6e4608e6bbc0df036
kasahata <kdot147x@gmail.com>
2025-07-07 18:51:03 -0700

    add starter page for dashboard and login page

---
+++ app/login/page.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Button } from "@/components/ui/button";
+
+export const metadata: Metadata = {
+  title: "Login",
+};
+
+export default function UserLoginPage() {
+  return (
+    <div>
+      <div className="flex flex-col content-center justify-center px-30 gap-3 min-h-screen bg-no-repeat bg-cover bg-[url(/patterns.png)]">
+        <form
+          action={""}
+          className="flex flex-col items-center justify-center bg-white rounded-2xl gap-3 pt-10 pb-10"
+        >
+          <h1 className="text-2xl">URL Shortener</h1>
+          <h2 className="text-xl">Log In</h2>
+          <div className="flex flex-col gap-2">
+            <label htmlFor=""></label>
+            <input type="text" name="email" id="" placeholder="Email" />
+            <input
+              type="password"
+              name="password"
+              id=""
+              placeholder="Password"
+            />
+            <Button>Login</Button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
