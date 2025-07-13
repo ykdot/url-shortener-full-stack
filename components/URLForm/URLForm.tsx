601929f9b3d29a28b3d362be2839446152e7dd40
ykdo <kdot147x@gmail.com>
2025-07-13 15:35:02 -0700

    made minor change to URLForm css

--- components/URLForm/URLForm.tsx
+++ components/URLForm/URLForm.tsx
@@ -1,10 +1,6 @@
 'use client';
 
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useForm } from 'react-hook-form';
-import { z } from 'zod';
-
-import { Button } from '@/components/ui/button';
+import { Button } from '../ui/button';
 import {
   Form,
   FormControl,
@@ -14,50 +10,56 @@ import {
   FormLabel,
   FormMessage,
 } from '@/components/ui/form';
-import { Input } from '@/components/ui/input';
+import { Input } from '../ui/input';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+// import { add_bp } from '@/actions/bp_db';
+import styles from './URLForm.module.css';
 
 const formSchema = z.object({
-  url: z.string().min(2, {
-    message: 'URL must be at least 2 characters.',
-  }),
+  url: z.string(),
 });
 
-export function URLForm() {
-  // 1. Define your form.
+function URLForm() {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
-    defaultValues: {
-      url: '',
-    },
   });
 
-  // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
+  async function onSubmit(values: z.infer<typeof formSchema>) {
     // ✅ This will be type-safe and validated.
-    console.log(values);
+    // const time = values.date.get('hour') + ':' + values.date.get('minute');
+    console.log(values.url);
+    // await add_bp(
+    //   values.date.toDate().toISOString(),
+    //   time,
+    //   values.systolic,
+    //   values.diastolic,
+    //   values.meds,
+    //   values.symptoms,
+    //   values.notes
+    // );
   }
-
   return (
-    <div className="p-10 w-3xl ">
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-          <FormField
-            control={form.control}
-            name="url"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>URL</FormLabel>
-                <FormControl>
-                  <Input placeholder="Enter URL" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <Button type="submit">Submit</Button>
-        </form>
-      </Form>
-    </div>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className={styles.form}>
+        <FormField
+          control={form.control}
+          name="url"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>URL</FormLabel>
+              <FormControl>
+                <Input placeholder="Enter URL" {...field} className="w-3xl" />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Add URL</Button>
+      </form>
+    </Form>
   );
 }
+
+export default URLForm;
