3b4873e16c3fff8c00df22e01d80af758b83b71f
ykdo <kdot147x@gmail.com>
2025-07-13 15:35:24 -0700

    made minor change to css

--- app/[username]/page.tsx
+++ app/[username]/page.tsx
@@ -1,18 +1,9 @@
 import type { Metadata } from 'next';
 import Link from 'next/link';
-import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from '@/components/ui/table';
-import { URLForm } from '@/components/URLForm/URLForm';
+import URLForm from '@/components/URLForm/URLForm';
 import styles from './page.module.css';
-import { Button } from '@/components/ui/button';
 import MainHeader from '@/components/MainHeader/MainHeader';
+import URLTable from '@/components/URLTable/URLTable';
 
 export const metadata: Metadata = {
   title: 'Homepage',
@@ -20,64 +11,12 @@ export const metadata: Metadata = {
 
 export default function UserPage() {
   return (
-    <div className={styles['container']}>
+    <>
       <MainHeader />
-      <URLForm />
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead>Date</TableHead>
-            <TableHead>New Link</TableHead>
-            <TableHead>Original Link</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          <TableRow>
-            <TableCell>7/12/25</TableCell>
-            <TableCell>
-              <Link
-                href="https://ui.shadcn.com/docs/components"
-                target="_blank"
-              >
-                https://ui.shadcn.com/docs/components
-              </Link>
-            </TableCell>
-            <TableCell>
-              <Link
-                href="https://ui.shadcn.com/docs/components"
-                target="_blank"
-              >
-                https://ui.shadcn.com/docs/components
-              </Link>
-            </TableCell>
-            <TableCell>
-              <Button>Delete URL</Button>
-            </TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>7/12/25</TableCell>
-            <TableCell>
-              <Link
-                href="https://ui.shadcn.com/docs/components"
-                target="_blank"
-              >
-                https://ui.shadcn.com/docs/components
-              </Link>
-            </TableCell>
-            <TableCell>
-              <Link
-                href="https://ui.shadcn.com/docs/components"
-                target="_blank"
-              >
-                https://ui.shadcn.com/docs/components
-              </Link>
-            </TableCell>
-            <TableCell>
-              <Button>Delete URL</Button>
-            </TableCell>
-          </TableRow>
-        </TableBody>
-      </Table>
-    </div>
+      <div className={styles['container']}>
+        <URLForm />
+        <URLTable />
+      </div>
+    </>
   );
 }
