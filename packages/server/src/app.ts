ffb8706ebf3332b54360488850e5624ce8f7c810
ykdo <kdot147x@gmail.com>
2025-07-18 18:27:29 -0700

    add test route for postgres and starting route for user

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -1,11 +1,23 @@
 import express, { Express, Request, Response } from 'express';
+import db from './db'; // Import the database module
 import usersRouter from './routes/users';
+
 
 const app: Express = express();
 const port = 3000;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, TypeScript Express! 👋');
+});
+
+app.get('/test-db', async (req: Request, res: Response): Promise<void> => {
+  try {
+    const result = await db.query('SELECT NOW()'); // Use the imported query function
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error connecting to the database');
+  }
 });
 
 app.use('/users', usersRouter);
--- packages/server/src/routes/users.ts
+++ packages/server/src/routes/users.ts
@@ -6,4 +6,10 @@ router.get('/', (req: Request, res: Response) => {
   res.send('This is the users route');
 });
 
+router.post('/login', (req: Request, res: Response) => {
+  res.send('Logging in process');
+}); 
+
+
+
 export default router;
