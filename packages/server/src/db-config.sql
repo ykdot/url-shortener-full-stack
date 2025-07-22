f17b6f259530257063bdb4a65b3e7c9560e16c12
ykdo <kdot147x@gmail.com>
2025-07-22 16:39:21 -0700

    add db configuration and rough create user route

---
+++ packages/server/src/db-config.sql
@@ -0,0 +1,38 @@
+CREATE DATABASE url_shortener_database;
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  username VARCHAR(25) NOT NULL,
+  email VARCHAR(25) NOT NULL,
+  password VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE urls (
+  short_code VARCHAR(6) PRIMARY KEY,
+  user_id SERIAL NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+  long_url VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE clicks (
+  short_code VARCHAR(6) PRIMARY KEY,
+  timestamp TIMESTAMPTZ NOT NULL,
+  id_address VARCHAR(100) NOT NULL,
+  country VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE url_analytics (
+  short_code VARCHAR(6) PRIMARY KEY,
+  clicks BIGINT NOT NULL
+);
+
+
+SELECT table_name
+FROM information_schema.tables
+WHERE table_schema = 'url_shortener_database'
+ORDER BY table_name;
+
+SELECT current_database();
+
+INSERT INTO users (username, email, password) VALUES ('user1', 'user1@aoz.com', '123456789');
+
+SELECT * FROM users;
--- packages/server/src/routes/users.ts
+++ packages/server/src/routes/users.ts
@@ -12,18 +12,37 @@ router.post('/login', (req: Request, res: Response) => {
   res.send('Logging in process');
 }); 
 
-router.post('/create-test-user', async (req: Request, res: Response) => {
+router.post('/create-user', async (req: Request, res: Response) => {
   const { username, email, password } = req.body;
 
   const insertQuery = 'INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING *';
   try {
+    if (username.trim().length < 3) {
+      return res.status(400).json({ 
+        error: 'Username or Password is wrong.' 
+      });
+    }
+
+    if (password.trim().length < 3) {
+      return res.status(400).json({ 
+        error: 'Username or Password is wrong.' 
+      });
+    }
+
+    const userExistsQuery = 'SELECT * FROM users WHERE email = $1 OR username = $2';
+    const existingUser = await db.query(userExistsQuery, [email, username]);
+
+    if (existingUser.rows.length > 0) {
+      return res.status(409).json({ error: 'Username or email already exists.' });
+    }
+
     const saltRounds = 10;
     const hashedPassword = await bcrypt.hash(password, saltRounds);
     
     const values = [username, email, hashedPassword];
 
     const result = await db.query(insertQuery, values);
-    res.json(result.rows[0]);
+    res.status(201).json(result.rows[0]);
   } catch (err) {
     console.error(err);
     res.status(500).send('Error connecting to the database');
