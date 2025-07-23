2261e3a4a67b20cf3f15a520a79f56f189988495
ykdo <kdot147x@gmail.com>
2025-07-22 17:26:53 -0700

    create basic logic route

--- packages/server/src/routes/users.ts
+++ packages/server/src/routes/users.ts
@@ -8,9 +8,42 @@ router.get('/', (req: Request, res: Response) => {
   res.send('This is the users route');
 });
 
-router.post('/login', (req: Request, res: Response) => {
-  res.send('Logging in process');
-}); 
+router.post('/login', async (req: Request, res: Response) => {
+  const { username, password } = req.body;
+
+  try {
+    if (username.trim().length < 3) {
+      return res.status(400).json({ 
+        error: 'Username or Password is wrong.' 
+      });
+    }
+
+    if (password.trim().length < 3) {
+      return res.status(400).json({ 
+        error: 'Username or Password is wrong.' 
+      });
+    }
+    const findUserQuery = 'SELECT * FROM users WHERE username = $1';
+    const userResult = await db.query(findUserQuery, [username]);
+
+    if (userResult.rows.length === 0) {
+      return res.status(401).json({ error: 'Username or Password is wrong.' });
+    }
+
+    const user = userResult.rows[0];
+    const storedHashedPassword = user.password;
+
+    const isMatch = await bcrypt.compare(password, storedHashedPassword);
+
+    if (!isMatch) {
+      return res.status(401).json({ error: 'Username or Password is wrong.' });
+    }
+    console.log("it works");
+    res.status(201).json({username, password});
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error connecting to the database');
+}}); 
 
 router.post('/create-user', async (req: Request, res: Response) => {
   const { username, email, password } = req.body;
