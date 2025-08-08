2ca44c2e343cf8b4fc0b0c5dd584d8783fc41721
ykdo <kdot147x@gmail.com>
2025-08-07 20:51:01 -0700

    add functionality to logout from MainHeader

--- packages/client/components/MainHeader/MainHeader.tsx
+++ packages/client/components/MainHeader/MainHeader.tsx
@@ -1,30 +1,29 @@
 'use client';
-import { useContext } from 'react';
-import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 import Link from 'next/link';
 import styles from './MainHeader.module.css';
 import { Button } from '../ui/button';
+import { logoutUser } from '@/actions/authentication-actions';
 // import webIcon from '@/assets/blood-pressure-icon.png';
-// import { logout } from '@/actions/auth-action';
 
 function MainHeader() {
-  function signout() {
-    // logout();
+  const { push } = useRouter();
+  function logout() {
+    logoutUser();
     // userCtx.logout();
+    push('/login');
   }
 
   // future work when checking the context to see if user is signed in
-  const date: Date = new Date();
   return (
     <header className={styles.main}>
       <Link className={styles['logo-click']} href="/">
-        {/* <Image src={webIcon} width={35} alt="Blood Pressure Icon" priority /> */}
         <p>URL Shortener</p>
       </Link>
 
       <nav className={styles['nav-list']}>
         <ul>
-          <Button>Logout</Button>
+          <Button onClick={logout}>Logout</Button>
         </ul>
       </nav>
     </header>
