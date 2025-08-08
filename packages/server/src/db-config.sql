8cda226e772f5414fd40e4075ff0de18beac632c
ykdo <kdot147x@gmail.com>
2025-08-07 20:45:16 -0700

    update working sql file and commands

--- packages/server/src/db-config.sql
+++ packages/server/src/db-config.sql
@@ -9,9 +9,11 @@ CREATE TABLE users (
 
 CREATE TABLE urls (
   short_code VARCHAR(6) PRIMARY KEY,
+  date DATE,
   user_id SERIAL NOT NULL REFERENCES users(id) ON DELETE CASCADE,
   long_url VARCHAR(100) NOT NULL
 );
+DROP TABLE urls;
 
 CREATE TABLE clicks (
   short_code VARCHAR(6) PRIMARY KEY,
@@ -38,3 +40,8 @@ INSERT INTO users (username, email, password) VALUES ('user1', 'user1@aoz.com', 
 SELECT * FROM users;
 SELECT * FROM urls;
 DELETE FROM users WHERE id='1' AND username='user1';
+DELETE FROM users WHERE id='2';
+
+INSERT INTO urls (short_code, date, user_id, long_url) VALUES ('111aaa', 'today', 11, 'https://www.youtube.com/');
+INSERT INTO urls (short_code, date, user_id, long_url) VALUES ('111aab', 'today', 11, 'https://www.google.com/');
+INSERT INTO urls (short_code, date, user_id, long_url) VALUES ('111aac', 'today', 11, 'https://www.reddit.com/');
