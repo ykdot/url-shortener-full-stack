9608137c419983c55765f736a8e56c34a83efee2
ykdo <kdot147x@gmail.com>
2025-08-07 20:44:47 -0700

    add proxy connections for express routes in development

--- packages/client/next.config.ts
+++ packages/client/next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:3000/:path*',
+      },
+    ]
+  },};
 
 export default nextConfig;
