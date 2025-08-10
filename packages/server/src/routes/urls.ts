a622a3c1843382fc641d827cd589637e603f0186
ykdo <kdot147x@gmail.com>
2025-08-09 18:16:12 -0700

    create route that add url to database and create short_code for access to urls

--- packages/server/src/routes/urls.ts
+++ packages/server/src/routes/urls.ts
@@ -1,5 +1,8 @@
 import express, { Request, Response, Router } from 'express';
+import jwt from 'jsonwebtoken';
 import db from '../db'; 
+import { encodeBase64 } from 'bcryptjs';
+import encodeToBase62 from '../utils/encoding';
 
 const router: Router = express.Router();
 
@@ -18,6 +21,41 @@ router.get('/:id', async (req: Request, res: Response) => {
     return res.status(404).json({
       message: "not found!"
     }); 
+  }
+});
+
+router.post('/create-new-url', async (req: Request, res: Response) => {
+  const { url } = req.body;
+  const createNewUrlQuery = 'INSERT INTO urls (date, user_id, long_url) VALUES ($1, $2, $3) RETURNING id';
+  const assignShortCodeQuery = 'UPDATE urls SET short_code=$1 WHERE id=$2';
+
+  try {
+    const token = req.cookies.authToken;    
+
+    if (!token) {
+      return res.status(401).json({ message: 'Authentication token is required' });
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as { id: number; username: string };
+    const result = await db.query(createNewUrlQuery, ['today', decoded.id, url]);
+
+    if (result.rowCount === 0) {
+      return res.status(400).send('Something went wrong');
+    }
+
+    const short_code = encodeToBase62(result.rows[0].id);
+    const result2 = await db.query(assignShortCodeQuery, [short_code, result.rows[0].id]);
+
+    if (result2.rowCount === 0) {
+      return res.status(400).send('Something went wrong');
+    }
+    
+    return res.status(201).json({
+      message: 'Successfully created new short_code',
+    });
+  }catch(err) {
+    console.error(err);
+    res.status(500).send('Error connecting to the database');
   }
 });
 
