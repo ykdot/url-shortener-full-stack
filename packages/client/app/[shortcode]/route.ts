518ff9b31c84fa1a0a3b96a03bdca573c3c30556
ykdo <kdot147x@gmail.com>
2025-08-18 16:28:55 -0700

    send user-agent info to express when next js is fetching url info

--- packages/client/app/[shortcode]/route.ts
+++ packages/client/app/[shortcode]/route.ts
@@ -2,7 +2,13 @@ import { redirect } from 'next/navigation';
 
 export async function GET(  request: Request, { params }: { params: { shortcode: string } }) {
   const { shortcode } = await params;
-  const res = await fetch(`http://localhost:3000/urls/${shortcode}`);
+  const userAgent = request.headers.get('user-agent');
+  console.log(userAgent);
+  const res = await fetch(`http://localhost:3000/urls/${shortcode}`, {
+    headers: {
+      'User-Agent': userAgent || 'unknown',
+    },
+  });  
   const data = await res.json();
   if (res.status == 404) {
     return Response.json({ data })
