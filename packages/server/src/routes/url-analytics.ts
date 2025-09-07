eeb1d43a2906ad2125424a363df3ca17b61b6279
third <icekap49@gmail.com>
2025-09-06 20:44:51 -0700

    add authorization check for both url analytics routes

--- packages/server/src/routes/url-analytics.ts
+++ packages/server/src/routes/url-analytics.ts
@@ -19,14 +19,16 @@ router.get('/test', (req: Request, res: Response) => {
 router.get('/main/:days', async (req: Request, res: Response) => {
   let days = req.params.days;
   try {
-    // change to admin token, authorization
-    // const token = req.cookies.authToken;    
-    // if (!token) {
-    //   return res.status(401).json({ message: 'Authentication token is required' });
-    // }
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1]; // Format: "Bearer TOKEN"    
+    if (!token) {
+      return res.status(401).json({ message: 'Authentication token is required' });
+    }
 
-    // const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as { id: number; username: string };
-
+    const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as { id: number; username: string; role: string };
+    if (decoded.role != 'admin') {
+      return res.status(403).json({ message: 'Forbidden Access' });
+    }
     const getMainAnalyticsQuery = `WITH filtered_data AS (SELECT short_code, COUNT(*) AS row_count FROM clicks WHERE timestamp >= current_date - interval '${days} days' GROUP BY short_code) SELECT (SELECT SUM(row_count) FROM filtered_data) AS total_clicks, (SELECT COUNT(short_code) FROM filtered_data) AS distinct_short_codes, (SELECT short_code FROM filtered_data ORDER BY row_count DESC LIMIT 1) AS most_frequent_short_code; `;
 
     const result = await db.query(getMainAnalyticsQuery);
@@ -58,13 +60,16 @@ router.get('/url-table/:filter/:order/:page/:wordFilter', async (req: Request, r
   const pageLimit = 2;
   const offset = (parseInt(page)-1) * pageLimit;
   try {
-    // change to admin token, authorization
-    // const token = req.cookies.authToken;    
-    // if (!token) {
-    //   return res.status(401).json({ message: 'Authentication token is required' });
-    // }
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1]; // Format: "Bearer TOKEN"    
+    if (!token) {
+      return res.status(401).json({ message: 'Authentication token is required' });
+    }
 
-    // const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as { id: number; username: string };
+    const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as { id: number; username: string; role: string };
+    if (decoded.role != 'admin') {
+      return res.status(403).json({ message: 'Forbidden Access' });
+    }
 
     const getMainTableQuery = `SELECT date, long_url, urls.short_code, clicks FROM urls LEFT JOIN url_analytics ON urls.short_code = url_analytics.short_code WHERE long_url ILIKE '%${wordFilter}%' ORDER BY ${filter} ${order} LIMIT ${pageLimit} OFFSET ${offset};`;
 
