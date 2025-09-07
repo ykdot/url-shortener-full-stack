e5e07ed253e7b3f2718366a1ef59c6fd94488353
third <icekap49@gmail.com>
2025-09-06 20:14:27 -0700

    create route to get table of urls with different kinds of sorting and filtering mechanism

--- packages/server/src/routes/url-analytics.ts
+++ packages/server/src/routes/url-analytics.ts
@@ -46,4 +46,43 @@ router.get('/main/:days', async (req: Request, res: Response) => {
   }
 });
 
+router.get('/url-table/:filter/:order/:page/:wordFilter', async (req: Request, res: Response) => {
+  const filter = req.params.filter;
+  const order = req.params.order;
+  let wordFilter = req.params.wordFilter;
+  if (req.params.wordFilter == "none") {
+    wordFilter = "";
+  }
+  console.log(wordFilter);
+  const page = req.params.page;
+  const pageLimit = 2;
+  const offset = (parseInt(page)-1) * pageLimit;
+  try {
+    // change to admin token, authorization
+    // const token = req.cookies.authToken;    
+    // if (!token) {
+    //   return res.status(401).json({ message: 'Authentication token is required' });
+    // }
+
+    // const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as { id: number; username: string };
+
+    const getMainTableQuery = `SELECT date, long_url, urls.short_code, clicks FROM urls LEFT JOIN url_analytics ON urls.short_code = url_analytics.short_code WHERE long_url ILIKE '%${wordFilter}%' ORDER BY ${filter} ${order} LIMIT ${pageLimit} OFFSET ${offset};`;
+
+    const result = await db.query(getMainTableQuery);
+
+    if (result.rowCount === 0) {
+      return res.status(404).json({ message: 'Data not found.' });
+    }
+
+    return res.status(200).json({
+      message: 'Login successful!',
+      data: result.rows
+    });
+  } catch (err) {
+    if (err instanceof jwt.JsonWebTokenError) {
+      return res.status(403).json({ message: 'Invalid or expired token.' });
+    }
+  }
+});
+
 export default router;
